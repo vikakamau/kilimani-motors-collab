4f7b0aac2b86d990bc280ed11170bbdc6cd0206e
dickson4954 <dicksonmurithi155@gmail.com>
2024-05-08 12:28:27 +0300

    initial commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react"
+
 
 function Home(){
-    return(
+  const [pic,setpic] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/cars")
+   .then(res=>res.json())
+   .then(data=>{
+      setpic(data)
+    })
+  },[])
+
+
+    return (
         <div>
-            <h1>home</h1>
+    <h1 className="text-center">Welcome To Kilimani Motors Ltd</h1>
+    
+   {pic.map((img)=>(
+    <div key={img.id}>
+       
+     <img src= {img.homepage}className=""alt="" style={{ width: '1890px' }}/>
+     </div>
+   ))}
+     
         </div>
     )
+
 }
-export default Home
+export default Home;
