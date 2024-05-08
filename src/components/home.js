b9701431ff8af0b39de6d7411504aa2fe7d0da4c
dickson4954 <dicksonmurithi155@gmail.com>
2024-05-08 13:53:13 +0300

    initial commit

--- src/components/home.js
+++ src/components/home.js
@@ -20,8 +20,35 @@ function Home(){
      <img src={img.homepage} className="" alt="" style={{ width: '1890px' }}/>
 
    ))}
+   <div class="row bg-primary" >
+                <div class="col-md-4">
+                    <header>
+                        <h3 id="call">Call Us</h3>
+                    </header>
+                
+                    <a className="text-white" href="0705237806">
+                        <span>0705237806</span>
+                    </a>
+                    <header>
+                        <h3 >Email Us</h3>
+                    </header>
+                    <a href="vikakamau04@gmail.com" className="text-white">
+                        <span>KilimaniMotors@gmail.Com</span>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <header>
+                        <h3>Business Hours</h3>
+                    </header>
+                    <h5 id="num">Monday-Friday: 8:30am to 5:30pm</h5>
+                    <h5 id="num">Saturday: 9:00am to 5:00pm</h5>
+                    <h5 id="num">Sorry We are Closed On Sunday</h5>
+                </div>
+
+            </div>
      
         </div>
+        
     )
 
 }
