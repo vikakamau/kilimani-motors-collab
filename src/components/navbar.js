42a672e29b896bd51adad78840805256967ed7a0
victor kamau <vikakamau04@gmail.com>
2024-05-08 12:24:51 +0300

    commits

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,56 +1,63 @@
 import { Link } from "react-router-dom";
 
+
+
 function Navbar() {
   return (
     <div>
-      <nav classNameName="navbar navbar-expand-lg bg-body-tertiary">
-        <div classNameName="container-fluid">
-          <h1 classNameName="text-bold text-primary m-2">
-            Kilimani Motors Limited
-          </h1>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <h1 className="text-bold text-primary m-2">Kilimani Motors Limited</h1>
 
-          <div
-            classNameName="collapse navbar-collapse "
-            id="navbarSupportedContent"
-          >
-            <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
-              <div classNameName="m-2">
-                <li classNameName="nav-item">
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <div className="m-2">
+                <li className="nav-item">
                   <Link to="/">
-                    <h3>Home</h3>
+                    <h3>
+                      Home
+                    </h3>
                   </Link>
                 </li>
+                
               </div>
-              <div classNameName="m-2">
-                <li classNameName="nav-item">
-                  <Link to="/about">
-                    <h3>About Us</h3>
-                  </Link>
-                </li>
+              <div className="m-2">
+              
+              <li className="nav-item">
+                <Link to="/about">
+                <h3 >
+                  About Us
+                </h3>
+                </Link>
+              </li>
               </div>
-              <div classNameName="m-2">
-                <li classNameName="nav-item">
-                  <Link to="/available">
-                    <h3>List of available Cars</h3>
+              <div className="m-2">
+              <li className="nav-item">
+                <Link to="/available">
+                  <h3>
+                      List of available Cars
+                  </h3>
                   </Link>
-                </li>
+              </li>
               </div>
-              <div classNameName="m-2">
-                <li classNameName="nav-item">
-                  <Link to="/contacts">
-                    <h3>Contact Us</h3>
-                  </Link>
-                </li>
+              <div className="m-2">
+              <li className="nav-item">
+                <Link to="/contacts">
+                  <h3>
+                    Contact Us
+                  </h3>
+                </Link>
+              </li>
               </div>
             </ul>
-            <form classNameName="d-flex" role="search">
+            <form className="d-flex" role="search">
               <input
-                classNameName="form-control me-2"
+                className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button classNameName="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
             </form>
